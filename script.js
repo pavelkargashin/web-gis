837b15e3a62200bebef08e66085abfd07bdb1919
Pavel <34913984+pavelkargashin@users.noreply.github.com>
2025-06-21 09:05:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -35,10 +35,10 @@ const osmLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png
 }).addTo(map);
 
 // Добавление дополнительных слоев (например, спутниковый)
-const satelliteLayer = L.tileLayer('https://{s}.satellite.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '© OpenStreetMap contributors'
-});
+//const satelliteLayer = L.tileLayer('https://{s}.satellite.openstreetmap.org/{z}/{x}/{y}.png', {
+  //  maxZoom: 19,
+    //attribution: '© OpenStreetMap contributors'
+//});
 
 // Создание объектов для GeoJSON слоев
 let layer1, layer2;
@@ -76,8 +76,8 @@ Promise.all([
 ]).then(() => {
     // Управление слоями
     const baseMaps = {
-        "OSM": osmLayer,
-        "Спутник": satelliteLayer
+        "OSM": osmLayer//,
+        //"Спутник": satelliteLayer
     };
 
     const overlayMaps = {
